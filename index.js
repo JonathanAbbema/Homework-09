e4f365679104e386ae298900935a30941daeebb4
JonathanAbbema <abbema44@gmail.com>
2023-05-21 12:35:46 -0700

    Final changes to the index.js file

--- index.js
+++ index.js
@@ -1,13 +1,108 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+const fs = require('fs');
+// const generateMarkdown = require("./generateMarkdown");
+
+
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt( [
+    {
+        type: 'input', 
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: (value) => { if (value){return true} else {return 'Please input a value to continue'}} 
+    },
+    {
+        type: 'input', 
+        name: 'description',
+        message: 'describe your project',
+        validate: (value) => { if (value){return true} else {return 'Please input a value to continue'}}
+    },
+    {
+        type: 'input', 
+        name: 'Table_of_contents',
+        message: 'Please break down the table of contents related to your project',
+        validate: (value) => { if (value){return true} else {return 'Please input a value to continue'}}
+    },
+    {
+        type: 'input', 
+        name: 'Installtion',
+        message: 'how do you install the application',
+        validate: (value) => { if (value){return true} else {return 'Please input a value to continue'}} 
+    },
+    {
+        type: 'input', 
+        name: 'Usage',
+        message: 'How do you use the application?',
+        validate: (value) => { if (value){return true} else {return 'Please input a value to continue'}} 
+    },
+    {
+        type: 'list', 
+        name: 'licenses',
+        message: 'what license did you use for your project?',
+        choices: ['The MIT License', 'The GPL License', 'Apache License', 'N/A'],
+        validate: (value) => { if (value){return true} else {return 'Please input a value to continue'}} 
+    },
+    {
+        type: 'input', 
+        name: 'Contributors',
+        message: 'Who can contribute to your application?',
+        validate: (value) => { if (value){return true} else {return 'Please input a value to continue'}} 
+    },
+    {
+        type: 'input', 
+        name: 'Test',
+        message: 'How do you test the application?',
+        validate: (value) => { if (value){return true} else {return 'Please input a value to continue'}} 
+    },
+    {
+        type: 'input', 
+        name: 'FAQ',
+        message: 'What are some FAQs you would like to include as a resource for your application',
+        validate: (value) => { if (value){return true} else {return 'Please input a value to continue'}} 
+    }
+]
+
+).then(({
+    title,
+    description,
+    Table_of_contents,
+    Installtion,
+    Usage,
+    licenses,
+    Contributors,
+    Test,
+    FAQ
+})=>{
+const template = `# ${title}
+# description
+${description}
+## Table_of_contents
+${Table_of_contents}
+## Installtion
+${Installtion}
+## Usage
+${Usage}
+## licenses
+${licenses}
+## Contributors
+${Contributors}
+## Test
+${Test}
+## FAQ
+${FAQ}`;
+createReadme(title, template); 
+}
+);
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+function createReadme(fileName,data){
+    fs.writeFile('./' + fileName.toLowerCase() + '.md', data,(err) => {
+        if(err){
+            console.log(err)
+        }
+        console.log('Your Readme has been created!')
+    })
+}
